84289915e7b3c69e18e91a6c0ec4e43a2e7469a0
domicch <domicch@gmail.com>
2021-01-05 23:59:02 +0000

    Add skaffold workflow

---
+++ deploy_local.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# # declare variables
+# SHA=$(git rev-parse HEAD)
+
+# echo latest SHA = $SHA
+
+# # build docker images
+# docker build -t domicch/multi_docker_k8s_client:latest -t domicch/multi_docker_k8s_client:$SHA -f ./client/Dockerfile ./client
+# docker build -t domicch/multi_docker_k8s_server:latest -t domicch/multi_docker_k8s_server:$SHA -f ./server/Dockerfile ./server
+# docker build -t domicch/multi_docker_k8s_worker:latest -t domicch/multi_docker_k8s_worker:$SHA -f ./worker/Dockerfile ./worker
+
+# # push images to git hub
+# docker push domicch/multi_docker_k8s_client:latest
+# docker push domicch/multi_docker_k8s_client:$SHA
+
+# docker push domicch/multi_docker_k8s_server:latest
+# docker push domicch/multi_docker_k8s_server:$SHA
+
+# docker push domicch/multi_docker_k8s_worker:latest
+# docker push domicch/multi_docker_k8s_worker:$SHA
+
+# k8s stuff
+kubectl apply -f k8s/project
+kubectl apply -f k8s/project_local
+# in order for k8s to "detect" latest built image, we tag the image with version identical to Git commit hash value
+# So every time image is updated, kubectl will explicitly get latest image to deploy
+kubectl set image deployment/server-deployment server=domicch/multi_docker_k8s_server:latest
+kubectl set image deployment/client-deployment client=domicch/multi_docker_k8s_client:latest
+kubectl set image deployment/worker-deployment worker=domicch/multi_docker_k8s_worker:latest
---
+++ skaffold/readme.txt
@@ -0,0 +1,15 @@
+Purpose of Skaffold:
+In development stage, when a file is updated, the updated file can be directly injected into the running pods to reflect the change
+
+https://skaffold.dev/docs/getting-started 
+
+Step 1:
+Install Skaffold with the following command:
+brew install skaffold
+
+Step 2:
+Create skaffold.yaml file
+
+Step 3:
+Run this command:
+skaffold dev
